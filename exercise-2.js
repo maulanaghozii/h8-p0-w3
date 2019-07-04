013663df21520ad0aa3336b4379953bcd39f55e9
Maulana Ghozi Humama <ghozihz@gmail.com>
2019-07-04 10:56:13 +0700

    add file exercise 2 in directory master

---
+++ exercise-2.js
@@ -0,0 +1,11 @@
+function balikString(str){
+    arr = [];
+    for(index = 0; index < str.length; index++){
+        arr = str[index] + arr;
+    }
+    return arr;
+}
+
+console.log(balikString('Hello World'));
+console.log(balikString('Hello Function'));
+console.log(balikString('Hacktiv8'));
